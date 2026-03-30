adefb1cee246e6855737258ce4ccb1501ad153b9
jeack <web028web028@gmail.com>
2026-03-31 01:48:46 +0800

    Create CountRewardERC20.sol

---
+++ CountRewardERC20.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+contract CountRewardERC20 is ERC20 {
+    mapping(address=>uint256) public userCount;
+    constructor() ERC20("CountReward", "CNT") {_mint(msg.sender,1000000 ether);}
+    function transfer(address to, uint256 amt) public override returns(bool){
+        userCount[msg.sender]++;
+        if(userCount[msg.sender]%10==0)_mint(msg.sender,100 ether);
+        return super.transfer(to,amt);
+    }
+}
