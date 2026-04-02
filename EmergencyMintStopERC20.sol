59c7c302c97b6a6f1b0bb8ad021f5b534a0ad399
jeack <web028web028@gmail.com>
2026-04-02 11:56:03 +0800

    Update EmergencyMintStopERC20.sol

--- EmergencyMintStopERC20.sol
+++ EmergencyMintStopERC20.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+
 contract EmergencyMintStopERC20 is ERC20, Ownable {
     bool public mintEnabled = true;
     constructor() ERC20("EmergencyMintStop", "EMS") {}
